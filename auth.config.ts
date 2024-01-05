bbabf562b9f62b093331cb3cfe532d2ab38e639d
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2024-01-04 22:00:25 -0300

    implement oauth

--- auth.config.ts
+++ auth.config.ts
@@ -4,9 +4,19 @@ import credentials from "next-auth/providers/credentials";
 
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "./data/user";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
